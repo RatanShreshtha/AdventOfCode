456bc8a15af7de238d9abfab6aded381e469279b
Ratan Kulshreshtha <ratan.shreshtha@gmail.com>
2023-12-03 22:40:13 +0530

    Day 1: Trebuchet?!, Part Two

--- 1_trebuchet.py
+++ 1_trebuchet.py
@@ -1,20 +1,56 @@
 total = 0
-digits = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+digits_in_words = {
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
 
-with open('./data/1_calibration_document.txt', 'r') as f:
+with open("./data/1_calibration_document.txt", "r") as f:
     for line in f:
-        number = ''
+        n = len(line)
+        number = ""
 
-        for left in line:
-            if left in digits:
-                number += left
+        stop = False
+
+        for i in range(n):
+            if stop:
                 break
-        
-        for right in line[::-1]:
-            if right in digits:
-                number += right
+
+            for j in range(i, n):
+                if line[i:j] in digits_in_words:
+                    digit = digits_in_words[line[i:j]]
+                    number += str(digit)
+                    stop = True
+                    break
+
+        stop = False
+        for i in range(n, -1, -1):
+            if stop:
                 break
-        
+
+            for j in range(n, -1, -1):
+                if line[j:i] in digits_in_words:
+                    digit = digits_in_words[line[j:i]]
+                    number += str(digit)
+                    stop = True
+                    break
+
         total += int(number)
 
 print(total)
